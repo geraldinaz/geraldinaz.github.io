196d2587f9c6b14f47cc7a1682f6602fc0956248
Geraldina <zhanggeraldina@gmail.com>
2020-07-31 17:05:04 -0400

    basics finished

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-window.onscroll = function() {myFunction()};
-var header = document.getElementById("myNav");
-var sticky = header.offsetTop;
-function myFunction() {
-    if (windows.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    }
-    else {
-        header.classList.remove("sticky");
-    }
-}
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("nav");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
